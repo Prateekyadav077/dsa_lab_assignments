e41ba211d6c5dcdc3b0d8e6ae3af9a13c738ac30
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 21:34:17 +0530

    Update Q_4.cpp

--- Ds-Assignment-2/Q_4.cpp
+++ Ds-Assignment-2/Q_4.cpp
@@ -42,10 +42,10 @@ void sortstring(char str[]) {
     while (str[len] != '\0') len++;
     for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
-            if (str[i] > str[j]) {
-                char temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
+       if (str[i] > str[j]) {
+       char temp = str[i];
+         str[i] = str[j];
+        str[j] = temp;
             }
         }
     }
@@ -67,17 +67,9 @@ int main() {
     char str1[100] = "Hello";
     char str2[50] = "World";
     concatenate(str1, str2);
-
-    
     reversestring(str1);
-
-    
-    deletevowels(str2);
-
-    
-    sortstring(str2);
-
-    
+    deletevowels(str2); 
+    sortstring(str2); 
     Lower(str2);
 
     return 0;
