4a40ea1b4ef11d456aa92b6798a268a18c443694
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:09:43 +0530

    Create Q_8.cpp

---
+++ Ds-Assignment-2/Assignment/Q_8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int count_distinct(char arr[], int n) {
+    int count = 0;
+    for (int i = 0; i<n; i++) {
+        bool duplicate = false;
+        for (int j=0; j<i; j++) {
+        if (arr[i]==arr[j]) {
+        duplicate = true;
+        break;
+            }
+        }
+        if (!duplicate) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    char arr[1000];
+    cout<<"enter string: ";
+    cin >> arr;
+    int n = 0;
+    while (arr[n]!='\0') {
+        n++;
+    }
+    cout <<count_distinct(arr, n) << endl;
+    return 0;
+}
