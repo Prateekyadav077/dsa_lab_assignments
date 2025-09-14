fb4c6bed818d78e72624293dcd62cf1175899295
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 14:12:58 +0530

    Update Q_2.cpp

--- DS-assignment-3/Q_2.cpp
+++ DS-assignment-3/Q_2.cpp
@@ -1,50 +1,60 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
+#include<string>
 class Stack{
- public:
-char *arr;
- int top;
-int size;
-
- Stack(int s){
- size=s;
-arr=new char[size];
- top=-1;
- }
-
- void push(char x){
- if(top==size-1){
-   cout<<"Overflow\n";
-}
- else{
-   arr[++top]=x;
- }
- }
-
- char pop(){
- if(top==-1){
-   return '\0';
-   }
- else{
-  return arr[top--];
- }
- }
+    
+    int size;
+    char *arr;
+    int top;
+    
+    public:
+    
+    Stack(int size){
+        this->size = size;
+         top =-1;
+        arr = new char[size];
+    }
+    
+    void push(char value){
+        if(top==size-1){
+            cout<<"full";
+            return;
+        }
+        
+        top++;
+        arr[top]=value;
+        
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"\nempty";
+            return;
+            
+        }
+        top--;
+    }
+    void peek(){
+        if(top==-1){
+            cout<<"\nempty";
+            return;
+        }
+        cout<<arr[top];
+    }
 };
 
- int main(){
- int n;
- cout<<"Enter length: ";
- cin>>n;
- char str[100];
- cout<<"Enter string: ";
- for(int i=0;i<n;i++){cin>>str[i];}
+int main() {
+    // Write C++ code here
+    Stack s(13);
+    string c= "DataStructure";
+    
+    for(int i=0;i<13;i++){
+    
+        s.push(c[i]);
+    }
+     for(int i=0;i<13;i++){
+        s.peek();
+        s.pop();
+    }
 
- Stack s(n);
- for(int i=0;i<n;i++){s.push(str[i]);}
-
- cout<<"Reversed: ";
- for(int i=0;i<n;i++){ cout<<s.pop(); }
- cout<<"\n";
- return 0;
- }
+    return 0;
+}
