5d2a371a3c444a38688848f1d59b0aadbc463ede
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 21:26:40 +0530

    Update Q_3.cpp

--- Ds-Assignment-2/Q_3.cpp
+++ Ds-Assignment-2/Q_3.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
-
-int findMissing(int arr[], int n) {
+int linearSearch(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+     if (arr[i] != i+1) {
+    return i+1;
+ }
+    }
+    return n; 
+}
+int binary(int arr[], int n) {
     int left = 0, right = n - 1;
     while (left <= right) {
         int mid = (left + right) / 2;
@@ -18,6 +25,7 @@ int findMissing(int arr[], int n) {
 int main() {
     int arr[] = {1, 2, 3, 4, 6, 7, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << "Missing number is: " << findMissing(arr, n) << endl;
+    cout << "Missing number using linear search is: " << linear(arr, n) << endl;
+    cout << "Missing number using binary serch is: " << binary(arr, n) ;
     return 0;
 }
