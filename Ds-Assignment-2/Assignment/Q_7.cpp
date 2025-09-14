d8e80aebdfc0d6d861105ba430f3954fa6dbe7a5
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:08:22 +0530

    Create Q_7.cpp

---
+++ Ds-Assignment-2/Assignment/Q_7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter length ";
+    cin >> n;
+    int arr[n];
+    cout<<"enter element "<<endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int inversions = 0;
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            if (arr[i] > arr[j])
+                inversions++;
+        }
+    }
+
+    cout << inversions << endl;
+
+    return 0;
+}
