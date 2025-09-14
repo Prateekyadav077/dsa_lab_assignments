a919dad2bb4f243af1601232f0f21056cf57df13
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:20:29 +0530

    Update Ques_5.cpp

--- DS_Assignment_1/Ques_5.cpp
+++ DS_Assignment_1/Ques_5.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 void rowSum(int a[10][10], int r, int c) {
     for (int i = 0; i < r; i++) {
-        int sum = 0;
-        for (int j = 0; j < c; j++) {
-            sum += a[i][j];
+    int sum = 0;
+    for (int j = 0; j < c; j++) {
+    sum += a[i][j];
         }
         cout << "Sum of row " << i + 1 << " = " << sum << endl;
     }
 }
 
 void colSum(int a[10][10], int r, int c) {
-    for (int j = 0; j < c; j++) {
-        int sum = 0;
-        for (int i = 0; i < r; i++) {
-            sum += a[i][j];
+  for (int j = 0; j < c; j++) {
+      int sum = 0;
+  for (int i = 0; i < r; i++) {
+    sum += a[i][j];
         }
         cout << "Sum of col " << j + 1 << " = " << sum << endl;
     }
@@ -28,7 +28,7 @@ int main() {
     cout << "Enter matrix elements:" << endl;
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            cin >> a[i][j];
+         cin >> a[i][j];
         }
     }
         cout<<"entered matrix:"<<endl;
