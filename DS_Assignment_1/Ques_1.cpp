3c24d8e907683b087db4a9efc214bfd33455e972
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:17:24 +0530

    Update Ques_1.cpp

--- DS_Assignment_1/Ques_1.cpp
+++ DS_Assignment_1/Ques_1.cpp
@@ -6,10 +6,8 @@ int arr[100], size_array = 0;
 void createArray() {
     cout << "Enter size: ";
     cin >> size_array;
-  
-    for (int i = 0; i < size_array; i++) {
-        
-          cout << "Enter element "<< i+1<<" : ";
+  for (int i = 0; i < size_array; i++) {
+      cout << "Enter element "<< i+1<<" : ";
         cin >> arr[i];
 
     }
@@ -21,7 +19,6 @@ void displayArray() {
     }
     cout << "\n";
 }
-
 void insertElement() {
     int pos, value;
     cout << "Enter position and value: ";
@@ -31,9 +28,9 @@ void insertElement() {
         return;
     }
     for (int i = size_array; i >= pos; i--) {
-        arr[i] = arr[i - 1];
+        arr[i] = arr[i-1];
     }
-    arr[pos - 1] = value;
+    arr[pos-1] = value;
     size_array++;
 }
 
@@ -57,8 +54,8 @@ void linearSearch() {
     cin >> key;
     for (int i = 0; i < size_array; i++) {
         if (arr[i] == key) {
-            cout << "Found at position " << i + 1 << "\n";
-            found = 1;
+         cout << "Found at position " << i + 1 << "\n";
+        found = 1;
         }
     }
     if (found=0) cout << "Not found\n";
@@ -71,13 +68,13 @@ int main() {
         cout << "Enter choice: ";
         cin >> choice;
         switch (choice) {
-            case 1: createArray(); break;
-            case 2: displayArray(); break;
-            case 3: insertElement(); break;
-            case 4: deleteElement(); break;
-            case 5: linearSearch(); break;
-            case 6: cout << "Exiting...\n"; break;
-            default: cout << "Invalid choice\n";
+         case 1: createArray(); break;
+   case 2: displayArray(); break;
+     case 3: insertElement(); break;
+    case 4: deleteElement(); break;
+    case 5: linearSearch(); break;
+    case 6: cout << "Exiting...\n"; break;
+     default: cout << "Invalid choice\n";
         }
     } while (choice != 6);
 }
