22c84ff106c35c2631e8328c9a1bf88c8f1a1aae
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:19:00 +0530

    Update Ques_4_2.cpp

--- DS_Assignment_1/Ques_4_2.cpp
+++ DS_Assignment_1/Ques_4_2.cpp
@@ -4,21 +4,21 @@ using namespace std;
 void multiply_matrix(int a[10][10], int b[10][10], int r1, int c1, int r2, int c2) {
     int c[10][10];
     for (int i = 0; i < r1; i++) {
-        for (int j = 0; j < c2; j++) {
-            c[i][j] = 0;
+    for (int j = 0; j < c2; j++) {
+   c[i][j] = 0;
         }
     }
     for (int i = 0; i < r1; i++) {
-        for (int j = 0; j < c2; j++) {
-            for (int k = 0; k < c1; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
+  for (int j = 0; j < c2; j++) {
+    for (int k = 0; k < c1; k++) {
+         c[i][j] += a[i][k] * b[k][j];
+       }
         }
     }
     cout << "Resultant Matrix:\n";
     for (int i = 0; i < r1; i++) {
-        for (int j = 0; j < c2; j++) {
-            cout << c[i][j] << " ";
+      for (int j = 0; j < c2; j++) {
+          cout << c[i][j] << " ";
         }
         cout << endl;
     }
