5e77c9ac29c06635a86a38bc1ee6a695655f2965
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 14:14:05 +0530

    Update Q_4.cpp

--- DS-assignment-3/Q_4.cpp
+++ DS-assignment-3/Q_4.cpp
@@ -1,67 +1,106 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 class Stack{
-public:
-    char *stk;
-    int top;
     int size;
-    Stack(int s){
-        size = s;
-        stk = new char[size];
+    char* arr;
+    int top;
+    public:
+    Stack(int size){
+        this->size = size;
+        arr = new char[size];
         top = -1;
     }
-    void push(char x){
-        if(top == size-1){ cout<<"Overflow\n"; return; }
-        stk[++top] = x;
+    void push(int value){
+        if(top==size-1){
+            cout<<"full";
+            return;
+        }
+        top++;
+        arr[top]=value;
+        
     }
-    char pop(){
-        if(top == -1){ cout<<"Underflow\n"; return '\0'; }
-        return stk[top--];
+    void pop(){
+        if(top==-1){
+            cout<<"\nempty";
+            return;
+        }
+        top--;
     }
-    char peek(){ return (top==-1)?'\0':stk[top]; }
-    bool empty(){ return top==-1; }
-    ~Stack(){ delete[] stk; }
+    char topp(){
+        if(top==-1){
+        cout<<"\n empty";
+        return '\0';
+    }
+    return arr[top];
+    }
+    
+    bool empty(){
+        if(top==-1){
+            return true;
+        
+    }
+    else{return false;}
+    
+}
 };
-
-int precedence(char op){
-    if(op=='+'||op=='-') return 1;
-    if(op=='*'||op=='/') return 2;
-    if(op=='^') return 3;
-    return 0;
+int priority(char c){
+    if(c=='^'){
+        return 3 ;
+    }
+    else if(c=='*'||c=='/'){
+        return 2;
+    }
+    else if(c=='+'||c=='-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+void infixtopostfix(string s){
+    int i = 0;
+    Stack s1(20);
+    string ans = "";
+    int n = s.size();
+    while(i<n){
+        if((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')){
+            ans = ans + s[i];
+        }
+        else if(s[i]=='('){
+            s1.push(s[i]);
+        }
+        else if(s[i]==')'){
+            while(!s1.empty()&& s1.topp() != '('){
+                ans= ans+s1.topp();
+                s1.pop();
+            }
+            s1.pop();
+            
+        }
+        else{
+            
+            while(!s1.empty()&& priority(s[i])<=priority(s1.topp())){
+                ans= ans+ s1.topp();
+                s1.pop();
+                
+            }
+            s1.push(s[i]);
+                }
+        i++;
+    }
+    while(!s1.empty()){
+        
+    ans = ans+s1.topp();
+    
+    s1.pop();
+    
+}
+cout<<ans;
 }
 
-
-bool isOp(char ch){
-    return ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^';
-}
 int main(){
-    string exp, res="";
-    cout<<"Enter infix expression: ";
-    cin>>exp;
-
-    Stack stk(exp.length());
-
-    for(int i=0;i<exp.length();i++){
-        char ch = exp[i];
-        if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')||(ch>='0' && ch<='9')){
-            res += ch; 
-        }
-        else if(ch=='(') stk.push(ch);
-        else if(ch==')'){
-            while(!stk.empty() && stk.peek()!='(')
-                res += stk.pop();
-            if(!stk.empty()) stk.pop(); 
-        }
-        else if(isOp(ch)){
-            while(!stk.empty() && precedence(stk.peek())>=precedence(ch))
-                res += stk.pop();
-            stk.push(ch);
-        }
-    }a+
-    while(!stk.empty()) res += stk.pop();
-
-    cout<<"Postfix expression: "<<res<<endl;
-    return 0;
+    string expression;
+    cin>>expression;
+    infixtopostfix(expression);
+    
 }
-
