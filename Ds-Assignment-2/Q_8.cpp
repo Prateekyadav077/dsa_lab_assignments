99bf2ea5ec2023346aa083195e8a2472d7ba7b2d
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 21:57:47 +0530

    Update Q_8.cpp

--- Ds-Assignment-2/Q_8.cpp
+++ Ds-Assignment-2/Q_8.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int count_distinct(char arr[], int n) {
     int count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i<n; i++) {
         bool duplicate = false;
-        for (int j = 0; j < i; j++) {
-            if (arr[i] == arr[j]) {
-                duplicate = true;
-                break;
+        for (int j=0; j<i; j++) {
+        if (arr[i]==arr[j]) {
+        duplicate = true;
+        break;
             }
         }
         if (!duplicate) {
@@ -23,7 +23,9 @@ int main() {
     cout<<"enter string: ";
     cin >> arr;
     int n = 0;
-    while (arr[n] != '\0') n++;
+    while (arr[n]!='\0') {
+        n++;
+    }
     cout <<count_distinct(arr, n) << endl;
     return 0;
 }
