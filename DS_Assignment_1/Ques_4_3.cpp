9588676b4ca71d6b9fe9a5118b837314ed289fe9
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-09-14 22:19:38 +0530

    Update Ques_4_3.cpp

--- DS_Assignment_1/Ques_4_3.cpp
+++ DS_Assignment_1/Ques_4_3.cpp
@@ -4,17 +4,17 @@ using namespace std;
 void transpose(int a[10][10], int r, int c) {
     int t[10][10];
     for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            t[j][i] = a[i][j];
+    for (int j = 0; j < c; j++) {
+    t[j][i] = a[i][j];
         }
     }
 
     cout << "Transpose of matrix is:" << endl;
     for (int i = 0; i < c; i++) {
         for (int j = 0; j < r; j++) {
-            cout << t[i][j] << " ";
+      cout << t[i][j] << " ";
         }
-        cout << endl;
+       cout << endl;
     }
 }
 
